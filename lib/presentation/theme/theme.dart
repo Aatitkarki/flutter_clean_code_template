57c7373b4455e5b13d62317dc9fb73ea0edc0a7e
aatitkarki <aatitkarki123@gmail.com>
2021-02-23 00:26:27 +0545

    added a small task

--- lib/presentation/theme/theme.dart
+++ lib/presentation/theme/theme.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'colors.dart';
 
+//TODO: THEME FOR THE APP
 class AppTheme {
   AppTheme._();
   static final appTheme = AppTheme._();
