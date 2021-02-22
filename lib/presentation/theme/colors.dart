294770d805dd3d2f47fc359bf5f419d650451390
aatitkarki <aatitkarki123@gmail.com>
2021-02-22 19:40:10 +0545

    full project structure for clean code architecture

---
+++ lib/presentation/theme/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color kDarkBlue = Color(0xff0f35bd);
+const Color kLightBlue = Color(0xff536453);
+const Color kViolet = Color(0xff0ce1ff);
+const Color kDarkBlack = Color(0xff181a1d);
+const Color kLightWhite = Color(0xffb4c6e1);
+const Color kPureWhite = Color(0xffffffff);
+const Color klightDark = Color(0xff212427);
+const Color kBackgroundColor = Color(0xff2F3337);
+const Color kSliderColor = Color(0xffC9992C);
